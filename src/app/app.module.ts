95faf8ba2ada33e5c77567c95d502f50ffc72801
Alif Rio Utama <alifrio75@gmail.com>
2022-06-23 04:36:44 +0700

    add test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { PickerModule } from '@ctrl/ngx-emoji-mart';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    PickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
